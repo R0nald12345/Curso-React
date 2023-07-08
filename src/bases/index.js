7d787e5389a1a92659954d8fc149ef1ce74b39f1
Ronald Camino P <pumaronal2016@gmail.com>
2023-07-07 21:14:36 -0400

    Mi primer commit intro js

---
+++ src/bases/index.js
@@ -0,0 +1,15 @@
+//Variables constantes y COnstantes
+
+const nombre = 'Fernando';
+const apellido = 'Herrera';
+
+let valorDado = 5;
+
+console.log(nombre, apellido, valorDado);
+
+//no se debe usar el (  var  )
+if(true){
+    const nombre = 'Peter';
+    let valorDado = 6;
+    console.log(valorDado, nombre);
+}
